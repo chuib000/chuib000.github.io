d73505973be53d7c41df89b607714c65af2bda7b
chuib000 <41131081+chuib000@users.noreply.github.com>
2018-07-14 00:29:39 -0700

    quote marks on list items

--- javascript.js
+++ javascript.js
@@ -1,19 +1,18 @@
 var likes = [
-  chocolate,
-  ice cream,
-  snickers,
-  Sean Lai,
-  climbing,
-  pineapple gummy bears,
-  orange juice,
-  mimosas,
-  crimps,
-  technical climbs,
-  slab climbs,
-  hiking
+  'chocolate',
+  'ice cream',
+  'snickers',
+  'Sean Lai',
+  'climbing',
+  'pineapple gummy bears',
+  'orange juice',
+  'mimosas',
+  'crimps',
+  'technical climbs',
+  'slab climbs',
+  'hiking'
 ]
 function newLike() {
 var randomNumber = Math.floor(Math.random() * (likes.length));
 document.getElementById('like').innerHTML = likes[randomNumber];
 }
-
