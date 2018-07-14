58835ec956d368c80e1876d6687bcfeb7dc22cf5
chuib000 <41131081+chuib000@users.noreply.github.com>
2018-07-14 00:33:58 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -12,7 +12,24 @@ var likes = [
   'slab climbs',
   'hiking'
 ]
+
 function newLike() {
 var randomNumber = Math.floor(Math.random() * (likes.length));
 document.getElementById('like').innerHTML = likes[randomNumber];
+}
+
+var dislikes = [
+  'cake that falls apart',
+  'bad drivers',
+  'slopers',
+  'Nazis',
+  'National Socialism',
+  'waking up early',
+  'trouble kids'
+]
+
+function newDislike() {
+var randomNumber = Math.floor(Math.random() *(dislikes.length));
+document.getElementById('dislike').innerHTML = dislikes[randomNumber];
+
 }
