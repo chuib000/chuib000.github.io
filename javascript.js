03aa9dce1a51619b8b838b7a4a6cc37e48725b34
chuib000 <41131081+chuib000@users.noreply.github.com>
2018-07-14 00:38:13 -0700

    dislikes and divs
    changed to getElementbById('_Display')

--- javascript.js
+++ javascript.js
@@ -15,7 +15,7 @@ var likes = [
 
 function newLike() {
 var randomNumber = Math.floor(Math.random() * (likes.length));
-document.getElementById('like').innerHTML = likes[randomNumber];
+document.getElementById('DislikeDisplay').innerHTML = likes[randomNumber];
 }
 
 var dislikes = [
@@ -30,6 +30,6 @@ var dislikes = [
 
 function newDislike() {
 var randomNumber = Math.floor(Math.random() *(dislikes.length));
-document.getElementById('dislike').innerHTML = dislikes[randomNumber];
+document.getElementById('DislikeDisplay').innerHTML = dislikes[randomNumber];
 
 }
